c8075344f0bce4e7892da5574a3b6d3a867646c0
Stylll <stephen.aribaba@yahoo.com>
2019-04-23 15:05:57 +0100

    chore(setup-client) Setup React Application

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# rpg-client
+A react app which consumes an API that generates a list of random phone numbers
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import ListViewPage from '../views/ListViewPage';
+
+const Routes = () => (
+  <Switch>
+      <Route
+          path="/"
+          exact
+          component={ListViewPage}
+      />
+  </Switch>
+)
+
+export default Routes;
